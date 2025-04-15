a5e553d9f81999f62159a617258f444b7d74a18a
Harsh1322 <65299748+Harsh1322@users.noreply.github.com>
2025-04-15 15:32:59 +0530

    Update app.py DB

--- micdrop-backend/app.py
+++ micdrop-backend/app.py
@@ -15,7 +15,7 @@ from urllib.parse import quote_plus as urlquote
 
 app = Flask(__name__)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://micdrop_71no_user:cSK215uK3rFGWk2V0aBz1LLpEBXIzOzG@dpg-cttbe723esus7392s410-a/micdrop_71no"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://micdropdb_user:EaVdTN7kci3W4QJYSYxVPKCT5ai9TjLZ@dpg-cvv2pc2dbo4c73fctjs0-a/micdropdb"
 db = SQLAlchemy(app)
 
 class Coordinator(db.Model):
